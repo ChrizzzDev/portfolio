e44d743f42aed79df0540e9eea0ce6cbc250c7b2
Chriz <123200043+ChrizzzDev@users.noreply.github.com>
2023-03-11 08:37:37 -0600

    🐈🐈🐈

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./style.css";
+import {
+  FaGithub,
+  FaDiscord,
+  FaWhatsapp,
+  FaInstagram,
+  FaYoutube,
+  FaTwitch,
+} from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        {socialprofils.github && (
+          <li>
+            <a href={socialprofils.github}>
+              <FaGithub />
+            </a>
+          </li>
+        )}
+        {socialprofils.discord && (
+          <li>
+            <a href={socialprofils.discord}>
+              <FaDiscord />
+            </a>
+          </li>
+        )}
+        {socialprofils.whatsapp && (
+          <li>
+            <a href={socialprofils.whatsapp}>
+              <FaWhatsapp />
+            </a>
+          </li>
+        )}
+        {socialprofils.youtube && (
+          <li>
+            <a href={socialprofils.youtube}>
+              <FaYoutube />
+            </a>
+          </li>
+        )}
+        {socialprofils.twitch && (
+          <li>
+            <a href={socialprofils.twitch}>
+              <FaTwitch />
+            </a>
+          </li>
+        )}
+        {socialprofils.instagram && (
+          <li>
+            <a href={socialprofils.instagram}>
+              <FaInstagram />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Sígueme</p>
+    </div>
+  );
+};
---
+++ src/content_option.js
@@ -0,0 +1,154 @@
+const logotext = "CHRIZZZ";
+const meta = {
+    title: "ChrizzzDev",
+    description: "Soy Hector Morett, desarrollador de Node.JS _ Desarrollador Full stack, actualmente estudiando.",
+};
+
+const introdata = {
+    title: "Soy",
+    animated: {
+        first: "github.com/ChrizzzDev",
+        second: "youtube.com/@chrizzzdev",
+        third: "Sueña con el corazón.",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    // description: "Hola, soy Hector. Este es mi sitio personal ",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Un poco acerca de mí",
+    aboutme_1: "Mi nombre es",
+    aboutme_2: "pero puedes llamarme 'Chriz' —sobrenombre—. Soy un desarrollador mexicano que basa su experiencia mayormente en JavaScript/TypeScript junto a Node.JS. Aunque cuento con la experiencia necesaria para conseguir un trabajo, sigo aprendiendo más cada día para ofrecer una mejor experiencia de usuario."
+};
+const worktimeline = [{
+        jobtitle: "Freelancer",
+        where: "Remoto",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "JavaScript/TypeScript",
+        value: 90,
+    },
+    {
+        name: "Node.JS",
+        value: 85,
+    },
+    {
+        name: "Python",
+        value: 40,
+    },
+    {
+        name: "Ruby",
+        value: 60,
+    },
+    {
+        name: "C#/.NET",
+        value: 85,
+    },
+    {
+        name: "MongoDB",
+        value: 95
+    }
+];
+
+const services = [{
+    title: "Freelancer",
+    description: "Actualmente trabajo como 'Freelancer' en varios lenguajes."
+   }
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "chrizdev4@gmail.com",
+    YOUR_FONE: "+52 341-170-5637",
+    description: "Si deseas contactar conmigo, aquí tienes información.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_r7mujmh",
+    YOUR_TEMPLATE_ID: "template_ykv0mcs",
+    YOUR_USER_ID: "543wbYQcuOB9MSVF5",
+};
+
+const socialprofils = {
+    github: "https://github.com/ChrizzzDev",
+    discord: "https://discord.com/users/947156433634488360",
+    whatsapp: "https://api.whatsapp.com/send?phone=523411705637",
+    instagram: "https://www.instagram.com/kraster.__"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
